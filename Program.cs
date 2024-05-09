e90987955f45ad1840a7efccfb4aad8fecc3f232
derpe29 <ruslanvitaliev@gmail.com>
2024-05-09 17:39:17 -0400

    Create Portfolio

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using somelern.Components;
+//для файлов
+using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+//для файлов
+builder.Services.Configure<StaticFileOptions>(options =>
+{
+    options.ContentTypeProvider = new FileExtensionContentTypeProvider
+    {
+        Mappings =
+        {
+            [".gltf"] = "model/gltf+json",
+            [".glb"] = "model/gltf-binary",
+            [".bin"] = "application/octet-stream",
+            [".obj"] = "model/obj",
+            [".fbx"] = "application/fbx"
+        }
+    };
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
